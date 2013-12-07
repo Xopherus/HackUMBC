6a4d2b429885fe90b11a271bf64b5687c130143c
Robertocat <jrobert1@umbc.edu>
2013-12-06 21:13:19 -0500

    update

--- hackumbc.user.js
+++ hackumbc.user.js
@@ -3,8 +3,11 @@
 // @namespace  http://www.umbc.edu
 // @version    0.1
 // @description  Langauge Plugin
-// @include      *
+// @include      *pastebin*
 // @copyright  2013, Robert Jackson, Christopher Raborg, Zach Hisley
 // ==/UserScript==
 
-alert('Hi')
+var chosenWords = document.getElementsByTagName('html')[0].innerHTML.replace(/(<([^>]+)>)/ig,"").split(" ")
+
+console.log(chosenWords.length);
+console.log(chosenWords[0]);
